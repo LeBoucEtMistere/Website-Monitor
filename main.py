beccbf60d74818d2137dd315a46c7b6fbc5845c4
Depasse Arthur <arthur.depasse96@gmail.com>
2019-11-11 17:15:57 +0000

    moved to argparse instead of raw input

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import validators
-from monitor import Monitor
-from stats import Stats
-from display import StatDisplay
-from alert import Alert
 from pipeline import Pipeline
-from concurrent.futures import ThreadPoolExecutor
 import signal
+import argparse
 
 
 class ServiceExit(Exception):
@@ -23,28 +19,45 @@ def service_shutdown(signum, frame):
 
 def main():
 
-    print("Website monitoring tool")
-    url = input("please enter the url of the website you want to monitor: ")
-    if not validators.url(url) and not validators.domain(url):
-        print("Url entered is not a valid url or doamin name. Exiting the program.")
+    parser = argparse.ArgumentParser(prog='main.py',
+                                     usage='%(prog)s [options] websites',
+                                     description='Monitor a given website at a given frequency and prints useful stats about it.')
+
+    parser.add_argument('websites',
+                        metavar='websites',
+                        type=str,
+                        help='a list of websites separated by commas')
+    parser.add_argument('-i',
+                        '--interval',
+                        help='the interval in seconds at which the sites must be monitored',
+                        nargs='?', const=1, type=int)
+    args = parser.parse_args()
+    print(args)
+
+    urls = args.websites.split(',')
+    interval = int(args.interval)
+
+    if interval <= 0:
+        print('Invalid negative or null interval time used')
         return
-    if not validators.url(url):
-        url = 'http://' + url
-    interval = input("please enter the checking interval in seconds: ")
-    try:
-        interval = int(interval)
-        assert(interval > 0)
-    except ValueError:
-        print("The input is not a valid integer. Exiting the program. ")
-        return
-    except AssertionError:
-        print("The input is not a strictly positive integer. Exiting the program. ")
-        return
+
+    for url in urls:
+
+        if not validators.url(url) and not validators.domain(url):
+            print(
+                "Url entered is not a valid url or domain name ({}). Exiting the program.".format(url))
+            return
+        if not validators.url(url):
+            url = 'http://' + url
+
+    print(urls)
+    print(interval)
 
     signal.signal(signal.SIGTERM, service_shutdown)
     signal.signal(signal.SIGINT, service_shutdown)
-
+    print("Starting the monitoring process, press CTRL+C to exit program.")
     try:
+
         p = Pipeline(url, interval)
         p.add_stat(120, 10, True)
         p.add_stat(600, 60, False)
